daeee5c634db5442c85504e6d2aadd11834bcee1
Durga Priya <durgapriya.535@gmail.com>
2024-06-22 23:15:19 +0530

    Removing deprecated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import ArticlePage from './Pages/ArticlePage';
+import './App.css';
 
 function App() {
   return (
-    <Router >
+    <Router basename="/assignment_newsportal">
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage/>} />
